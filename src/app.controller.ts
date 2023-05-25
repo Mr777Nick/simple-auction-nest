6fc0062080c3d0b5f0a54ccac0dcac6288aa5c5d
Bagas Naufal Insani <bagas_naufal96@yahoo.co.id>
2023-05-25 19:31:05 +0700

    feat: add auto openapi docs using swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,19 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiBasicAuth, ApiOkResponse } from '@nestjs/swagger';
 
 import { AppService } from './app.service';
 
+@ApiBasicAuth()
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  /**
+   * Get Hello World message, commonly used for checking if the server is running
+   * @returns {string} Hello World message
+   */
   @Get()
+  @ApiOkResponse({ content: { 'text/html': { schema: { type: 'string' } } } })
   getHello(): string {
     return this.appService.getHello();
   }
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,24 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Simple Auction API')
+    .setDescription('The backend API for the Simple Auction app')
+    .setVersion('1.0')
+    .setContact(
+      'Bagas Naufal Insani',
+      'https://github.com/Mr777Nick',
+      'bagas_naufal96@yahoo.co.id',
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3000);
 }
 bootstrap();
