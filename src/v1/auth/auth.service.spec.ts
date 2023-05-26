700ed396fe4c9c03f92f15dd8f356b3f0e9970cb
Bagas Naufal Insani <bagas_naufal96@yahoo.co.id>
2023-05-27 02:19:36 +0700

    test: update auth module spec tests

--- src/v1/auth/auth.service.spec.ts
+++ src/v1/auth/auth.service.spec.ts
@@ -1,13 +1,32 @@
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { SupabaseModule } from '../../common/supabase/supabase.module';
+import { UsersModule } from '../users/users.module';
+
+import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { jwtConstants } from './constants/constants';
+import { JwtStrategy } from './strategy/jwt.strategy';
+import { LocalStrategy } from './strategy/local.strategy';
 
 describe('AuthService', () => {
   let service: AuthService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
+      imports: [
+        UsersModule,
+        PassportModule,
+        JwtModule.register({
+          secret: jwtConstants.secret,
+          signOptions: { expiresIn: '60s' },
+        }),
+        SupabaseModule,
+      ],
+      controllers: [AuthController],
+      providers: [AuthService, LocalStrategy, JwtStrategy],
     }).compile();
 
     service = module.get<AuthService>(AuthService);
