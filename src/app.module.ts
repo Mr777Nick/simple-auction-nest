063996a9c4357c53292745ea40df52dec3a68ced
Bagas Naufal Insani <bagas_naufal96@yahoo.co.id>
2023-05-27 23:03:08 +0700

    feat: add typeorm connection to postgresql db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { APP_INTERCEPTOR } from '@nestjs/core';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostgresTypeOrmConfigService } from './common/database/postgres-typeorm-config-service';
 import { ResponseTransformationInterceptor } from './common/interceptor/transform-response.interceptor';
 import { SupabaseModule } from './common/supabase/supabase.module';
 import { AuthModule } from './v1/auth/auth.module';
@@ -14,6 +16,11 @@ import { AuthModule } from './v1/auth/auth.module';
     SupabaseModule,
     ConfigModule.forRoot({
       envFilePath: ['.env.development.local', '.env.development'],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useClass: PostgresTypeOrmConfigService,
     }),
   ],
   controllers: [AppController],
---
+++ src/common/database/postgres-typeorm-config-service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { TypeOrmModuleOptions, TypeOrmOptionsFactory } from '@nestjs/typeorm';
+
+@Injectable()
+export class PostgresTypeOrmConfigService implements TypeOrmOptionsFactory {
+  constructor(private configService: ConfigService) {}
+
+  createTypeOrmOptions(): TypeOrmModuleOptions {
+    return {
+      type: 'postgres',
+      host: this.configService.get<string>('POSTGRES_HOST'),
+      port: +this.configService.get<string>('POSTGRES_PORT'),
+      username: this.configService.get<string>('POSTGRES_USER'),
+      password: this.configService.get<string>('POSTGRES_PASSWORD'),
+      database: this.configService.get<string>('POSTGRES_DATABASE'),
+      entities: [],
+      synchronize: false,
+    };
+  }
+}
