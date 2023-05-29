daf8933fc9aa77949e88fda961c77aaa832170ec
Bagas Naufal Insani <bagas_naufal96@yahoo.co.id>
2023-05-30 02:08:38 +0700

    test: fix item bids unit testings

--- src/v1/item-bids/item-bids.controller.spec.ts
+++ src/v1/item-bids/item-bids.controller.spec.ts
@@ -1,5 +1,12 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { ThrottlerModule } from '@nestjs/throttler';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
+import { repositoryMockFactory } from '../../../test/test-util';
+import { ItemsService } from '../items/items.service';
+import { UsersService } from '../users/users.service';
+
+import { ItemBid } from './entities/item-bid.entity';
 import { ItemBidsController } from './item-bids.controller';
 import { ItemBidsService } from './item-bids.service';
 
@@ -9,7 +16,36 @@ describe('ItemBidsController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ItemBidsController],
-      providers: [ItemBidsService],
+      imports: [
+        ThrottlerModule.forRoot({}),
+        {
+          module: class FakeItemsModule {},
+          providers: [
+            {
+              provide: ItemsService,
+              useValue: {},
+            },
+          ],
+          exports: [ItemsService],
+        },
+        {
+          module: class FakeUsersModule {},
+          providers: [
+            {
+              provide: UsersService,
+              useValue: {},
+            },
+          ],
+          exports: [UsersService],
+        },
+      ],
+      providers: [
+        ItemBidsService,
+        {
+          provide: getRepositoryToken(ItemBid),
+          useFactory: repositoryMockFactory,
+        },
+      ],
     }).compile();
 
     controller = module.get<ItemBidsController>(ItemBidsController);
--- src/v1/item-bids/item-bids.service.spec.ts
+++ src/v1/item-bids/item-bids.service.spec.ts
@@ -1,5 +1,11 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
+import { repositoryMockFactory } from '../../../test/test-util';
+import { ItemsService } from '../items/items.service';
+import { UsersService } from '../users/users.service';
+
+import { ItemBid } from './entities/item-bid.entity';
 import { ItemBidsService } from './item-bids.service';
 
 describe('ItemBidsService', () => {
@@ -7,7 +13,35 @@ describe('ItemBidsService', () => {
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ItemBidsService],
+      imports: [
+        {
+          module: class FakeItemsModule {},
+          providers: [
+            {
+              provide: ItemsService,
+              useValue: {},
+            },
+          ],
+          exports: [ItemsService],
+        },
+        {
+          module: class FakeUsersModule {},
+          providers: [
+            {
+              provide: UsersService,
+              useValue: {},
+            },
+          ],
+          exports: [UsersService],
+        },
+      ],
+      providers: [
+        ItemBidsService,
+        {
+          provide: getRepositoryToken(ItemBid),
+          useFactory: repositoryMockFactory,
+        },
+      ],
     }).compile();
 
     service = module.get<ItemBidsService>(ItemBidsService);
